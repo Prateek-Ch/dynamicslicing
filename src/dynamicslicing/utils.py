7f9669bf9d3f36bd6ec9cc0bc64fa9f22433b88a
PrateekCh <prateek.chaturvedi216@gmail.com>
2023-11-24 00:01:24 +0100

    working changes

--- src/dynamicslicing/utils.py
+++ src/dynamicslicing/utils.py
@@ -1,8 +1,7 @@
-from typing import List, Union
+from typing import List, Union, Optional
 import libcst as cst
 from libcst._flatten_sentinel import FlattenSentinel
 from libcst._nodes.statement import BaseStatement, If
-from libcst._removal_sentinel import RemovalSentinel, RemoveFromParent
 from libcst.metadata import (
     ParentNodeProvider,
     PositionProvider,
@@ -19,7 +18,7 @@ class OddIfNegation(m.MatcherDecoratableTransformer):
         PositionProvider,
     )
 
-    def leave_If(self, original_node: If, updated_node: If) -> BaseStatement | FlattenSentinel[BaseStatement] | RemovalSentinel:
+    def leave_If(self, original_node: If, updated_node: If) -> BaseStatement | FlattenSentinel[BaseStatement] | cst.RemovalSentinel:
         location = self.get_metadata(PositionProvider, original_node)
         if location.start.line % 2 == 0:
             return updated_node
@@ -36,17 +35,28 @@ class RemoveLines(cst.CSTTransformer):
         PositionProvider,
         ParentNodeProvider,
     )
-    def __init__(self, lines_to_remove: set) -> None:
+    def __init__(self, lines_to_keep: set) -> None:
         super().__init__()
-        self.lines_to_remove = lines_to_remove
+        self.lines_to_keep = lines_to_keep
+    
+    def on_visit(self, node: cst.CSTNode) -> bool:
+        location = self.get_metadata(PositionProvider, node)
+        if int(location.start.line) in self.lines_to_keep:
+            return True
+        return False
     
     def on_leave(
-        self, original_node: cst.CSTNode, updated_node: cst.CSTNode
-    ) -> Union[cst.CSTNodeT, RemovalSentinel]:
+        self, original_node: cst.CSTNodeT, updated_node: cst.CSTNodeT
+    ) -> Union[cst.CSTNodeT, cst.RemovalSentinel]:
         location = self.get_metadata(PositionProvider, original_node)
-        if int(location.start.line) in self.lines_to_remove:
-            print(f"Removing line {location.start.line}")
-            return RemoveFromParent()
+        if int(location.start.line) not in self.lines_to_keep:
+            # print(f"Removing line {location.start.line}")
+            # try:
+            # create_empty_module()
+            return cst.RemoveFromParent()
+            # except Exception as e:
+                # create_empty_module()
+                # print("error")
         return updated_node
 
 def negate_odd_ifs(code: str) -> str:
@@ -56,14 +66,12 @@ def negate_odd_ifs(code: str) -> str:
     new_syntax_tree = wrapper.visit(code_modifier)
     return new_syntax_tree.code
 
-def remove_lines(code: str, lines_to_remove: List[int]) -> str:
+def remove_lines(code: str, lines_to_keep: List[int]) -> str:
     syntax_tree = cst.parse_module(code)
     wrapper = cst.metadata.MetadataWrapper(syntax_tree)
-    code_modifier = RemoveLines(lines_to_remove)
+    code_modifier = RemoveLines(lines_to_keep)
     new_syntax_tree = wrapper.visit(code_modifier)
-    print(new_syntax_tree.code)
     return new_syntax_tree.code
-
 
 # Example usage:
 original_code = """def slice_me():
@@ -77,6 +85,6 @@ original_code = """def slice_me():
 slice_me()
 """
 
-lines_to_remove = {1, 2, 4, 5, 9}
-
-remove_lines(original_code, lines_to_remove)
+lines_to_keep = {1, 2, 4, 5, 9}
+x = remove_lines(original_code, lines_to_keep)
+print(x)
