95e0fc02754a5f259924c6fc921e01a867d64f67
PrateekCh <prateek.chaturvedi216@gmail.com>
2023-11-23 01:31:01 +0100

    sort of task 2

--- src/dynamicslicing/utils.py
+++ src/dynamicslicing/utils.py
@@ -1,8 +1,8 @@
-from typing import List
+from typing import List, Union
 import libcst as cst
 from libcst._flatten_sentinel import FlattenSentinel
 from libcst._nodes.statement import BaseStatement, If
-from libcst._removal_sentinel import RemovalSentinel
+from libcst._removal_sentinel import RemovalSentinel, RemoveFromParent
 from libcst.metadata import (
     ParentNodeProvider,
     PositionProvider,
@@ -31,6 +31,24 @@ class OddIfNegation(m.MatcherDecoratableTransformer):
             test=negated_test,
         )
 
+class RemoveLines(cst.CSTTransformer):
+    METADATA_DEPENDENCIES = (
+        PositionProvider,
+        ParentNodeProvider,
+    )
+    def __init__(self, lines_to_remove: set) -> None:
+        super().__init__()
+        self.lines_to_remove = lines_to_remove
+    
+    def on_leave(
+        self, original_node: cst.CSTNode, updated_node: cst.CSTNode
+    ) -> Union[cst.CSTNodeT, RemovalSentinel]:
+        location = self.get_metadata(PositionProvider, original_node)
+        if int(location.start.line) in self.lines_to_remove:
+            print(f"Removing line {location.start.line}")
+            return RemoveFromParent()
+        return updated_node
+
 def negate_odd_ifs(code: str) -> str:
     syntax_tree = cst.parse_module(code)
     wrapper = cst.metadata.MetadataWrapper(syntax_tree)
@@ -39,4 +57,26 @@ def negate_odd_ifs(code: str) -> str:
     return new_syntax_tree.code
 
 def remove_lines(code: str, lines_to_remove: List[int]) -> str:
-    pass
+    syntax_tree = cst.parse_module(code)
+    wrapper = cst.metadata.MetadataWrapper(syntax_tree)
+    code_modifier = RemoveLines(lines_to_remove)
+    new_syntax_tree = wrapper.visit(code_modifier)
+    print(new_syntax_tree.code)
+    return new_syntax_tree.code
+
+
+# Example usage:
+original_code = """def slice_me():
+    x = 5
+    print("Hello World")
+    if x < 10:
+        x += 5
+    y = 0
+    return y
+
+slice_me()
+"""
+
+lines_to_remove = {1, 2, 4, 5, 9}
+
+remove_lines(original_code, lines_to_remove)
