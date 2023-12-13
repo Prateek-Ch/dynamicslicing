72d6c9b9c4fadc958126b2e19e27be9930089b86
PrateekCh <prateek.chaturvedi216@gmail.com>
2023-12-13 17:13:14 +0100

    temp working changes

--- src/dynamicslicing/slice_dataflow.py
+++ src/dynamicslicing/slice_dataflow.py
@@ -26,9 +26,12 @@ class SliceDataflow(BaseAnalysis):
             self.slice_criteria = set_slice_criterion[0]
             self.slicing_criterion_location = set_slice_criterion[1]
             self.dependencies = set()
+            self.node_dict = {}
             self.line_numbers = []
         
-    def add_node_to_dependencies(self, node: Any, location: int):
+    def add_node_to_dependencies(self, node: Any, location: int, type: str):
+        if location not in self.line_numbers:
+            self.node_dict[location] = {type: node}
         if isinstance(node, cst.Name):
             if node.value in self.slice_criteria and location not in self.line_numbers:
                 self.line_numbers.append(location)
@@ -46,7 +49,7 @@ class SliceDataflow(BaseAnalysis):
         location = self.iid_to_location(dyn_ast, iid)
         node = get_node_by_location(self._get_ast(dyn_ast)[0], location)
         if location.start_line == self.slicing_criterion_location:
-            self.add_node_to_dependencies(node, location.start_line)
+            self.add_node_to_dependencies(node, location.start_line, "read")
     
     def write(
         self, dyn_ast: str, iid: int, old_vals: List[Callable], new_val: Any
@@ -54,8 +57,8 @@ class SliceDataflow(BaseAnalysis):
         location = self.iid_to_location(dyn_ast, iid)
         node = get_node_by_location(self._get_ast(dyn_ast)[0], location)
         if location.start_line <= self.slicing_criterion_location:
-            self.add_node_to_dependencies(node, location.start_line)
-            
+            self.add_node_to_dependencies(node, location.start_line, "write")
+        
     def post_call(
         self,
         dyn_ast: str,
@@ -67,9 +70,11 @@ class SliceDataflow(BaseAnalysis):
     ) -> Any:
         location = self.iid_to_location(dyn_ast, iid)
         node = get_node_by_location(self._get_ast(dyn_ast)[0], location)
-        if node.func.value in self.slice_criteria and location.start_line not in self.line_numbers:
-            self.line_numbers.append(location.start_line)
-            self.dependencies.add(node)
+        if location.start_line not in self.line_numbers:
+            self.node_dict[location.start_line] = {"post_call": node}
+            if node.func.value in self.slice_criteria:
+                self.line_numbers.append(location.start_line)
+                self.dependencies.add(node)
     
     def function_enter(
         self, dyn_ast: str, iid: int, args: List[Any], name: str, is_lambda: bool
@@ -77,6 +82,7 @@ class SliceDataflow(BaseAnalysis):
         location = self.iid_to_location(dyn_ast, iid)
         node = get_node_by_location(self._get_ast(dyn_ast)[0], location)
         if location.start_line not in self.line_numbers:
+            self.node_dict[location.start_line] = {"function_enter": node}
             self.line_numbers.append(location.start_line)
             self.dependencies.add(node)
     
@@ -85,16 +91,47 @@ class SliceDataflow(BaseAnalysis):
     ):
         location = self.iid_to_location(dyn_ast, iid)
         node = get_node_by_location(self._get_ast(dyn_ast)[0], location)
+        func_value, func_attr = '', ''
         if location.start_line not in self.line_numbers:
+            self.node_dict[location.start_line] = {"pre_call": node}
             self.line_numbers.append(location.start_line)
             self.dependencies.add(node)
+        if isinstance(node, cst.Call):
+            node_func = node.func
+            if isinstance(node_func, cst.Attribute):
+                func_value = node_func.value
+                if isinstance(func_value, cst.Name):
+                    func_value = func_value.value
+                func_attr = node_func.attr
+                if isinstance(func_attr, cst.Name):
+                    func_attr = func_attr.value
+            
+        if func_value in self.slice_criteria and func_attr == 'append':
+            if node.args:
+                self.slice_criteria.add(node.args[0].value.value)
+    
+    def get_value(self, node) -> str:
+        if isinstance(node, cst.Name):
+            return node.value
+        elif isinstance(node, cst.Assign):
+            return node.targets[0].target.value
+        elif isinstance(node, cst.AugAssign):
+            return node.target.value
     
     def end_execution(self) -> None:
+        reverse_sorted_dict = dict(sorted(self.node_dict.items(), reverse = True))
+        for outer_key, inner_dict in reverse_sorted_dict.items():
+            for inner_key, value in inner_dict.items():
+                line_number = outer_key
+                dtype = inner_key
+                node = value
+                temp = self.get_value(node)
+                if temp in self.slice_criteria and line_number not in self.line_numbers and dtype != "read" and line_number <= self.slicing_criterion_location:
+                    self.line_numbers.append(line_number)
+                    if isinstance(node.value, cst.BinaryOperation):
+                        self.slice_criteria.add(node.value.left.value)
+                        self.slice_criteria.add(node.value.right.value)
         sliced_code = remove_lines(self.source, self.line_numbers)
         output_file_name = os.path.join(os.path.dirname(self.args.entry), "sliced.py")
         with open(output_file_name, "w") as output_file:
             output_file.write(sliced_code)
-    
- 
-
-# {linenumber: {read: write}}
